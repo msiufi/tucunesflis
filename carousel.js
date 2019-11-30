99339dbe5f78b55da57880bd6e471c3bc7c47117
guadampereyra <guadalupereyra@outlook.com>
2019-11-30 15:54:24 -0300

    actualizo home, agrego reg-form

--- carousel.js
+++ carousel.js
@@ -1,22 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.customer-logos').slick({
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 1500,
+      arrows: true,
+      dots: true,
+      pauseOnHover: false,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4
+        }
+      }, {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3,
+          arrows: false
+        }
+      }]
     });
-});
+  });
---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+   $('.up').hide(); 
+   
+   $(window).scroll(function(){
+       if($(this).scrollTop() > 500 ){
+        $('.up').fadeIn('1000');
+       } else{
+        $('.up').fadeOut('1000');
+       }
+   }
+   $('.up').click(function(){
+       $('body, html').animate({
+           scrollTop: 0
+       },150);
+       
+   })
+   ,)})
